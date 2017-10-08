04b4001e53902e75cc1cad8d894fcebc9484198c
Supitsara Prathan <supitsara@digio.co.th>
2017-10-08 22:48:05 +0700

    - create list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,112 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(new MyApp());
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
+
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Test Flutter Home Page'),
+  HomePageState createState() => new HomePageState();
+
+}
+
+class HomePageState extends State<HomePage> {
+
+  List data;
+
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull("https://api.coinmarketcap.com/v1/ticker/"),
+        headers: {
+          "Accept": "application/json"
+        }
     );
+
+    this.setState(() {
+      data = JSON.decode(response.body);
+    });
+    print(data[1]["name"]);
+
+    return "Success!";
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
+  void initState() {
+    this.getData();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  TextStyle isRaised(String priceChange) {
+    if(priceChange.substring(0, 1) == "-") {
+      return new TextStyle(color: Colors.red);
+    } else {
+      return new TextStyle(color: Colors.green);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("CryptoMarket"),
+        centerTitle: true,
       ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+      body: new ListView.builder(
+        addAutomaticKeepAlives: true,
+        itemCount: data == null ? 0 : data.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Card(
+            child: new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new ListTile(
+                  leading: new Image(
+                      image: new NetworkImage("https://files.coinmarketcap.com/static/img/coins/32x32/"+data[index]["name"].toString().toLowerCase().replaceAll(" ", "-")+".png", scale: 1.0)
+                  ),
+                  title: new Text(
+                    data[index]["rank"] + ". " + data[index]["name"],
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: new Container(
+                    child: new Row(
+                      children: <Widget>[
+                        new Text(
+                          "percent change 1h : ",
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        new Text(
+                          data[index]["percent_change_1h"] + "%",
+                          style: isRaised(data[index]["percent_change_1h"]),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                new ButtonTheme.bar( // make buttons use the appropriate styles for cards
+                  child: new ButtonBar(
+                    children: <Widget>[
+                      new MaterialButton(
+                        child: const Text('INFO'),
+                        onPressed: () { /* ... */ },
+                      ),
+                      new MaterialButton(
+                        child: const Text('FORECAST'),
+                        onPressed: () { /* ... */ },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            new Text(
-              '${_counter}',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
